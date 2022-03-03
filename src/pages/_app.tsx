b696397dc55f9f3de15910b1ef391448eb1b6ec7
João Vitor <infamoz_br3@hotmail.com>
2022-03-03 16:41:27 -0300

    remove gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import TagManager from 'react-gtm-module';
 
 import { appWithTranslation } from 'next-i18next';
-import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: "GTM-PWJ5J5W"});
-  }, []);
+  const router = useRouter();
 
-  return (
-    <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />
 }
 
 export default appWithTranslation(MyApp);
--- src/pages/cursos/[slug].tsx
+++ src/pages/cursos/[slug].tsx
@@ -2,7 +2,6 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { ParsedUrlQuery } from "querystring";
-import TagManager from "react-gtm-module";
 import { baseEN, basePT } from "../../services/airtable";
 
 interface IParams extends ParsedUrlQuery {
@@ -18,18 +17,7 @@ interface cursosProps {
 
 export default function Cursos({ data }: cursosProps) {
   const { t } = useTranslation('course');
-
-  const tagManagerArgs = {
-    dataLayer: {
-      contentId: "ed123",
-      contentName: `MBA em teste`,
-      currency: 'BRL',
-      value: "R$ 1.000.000",
-    }
-  };
-
-  TagManager.dataLayer(tagManagerArgs);
-
+ 
   return (
     <>
       <h1>{t("title")}</h1>
